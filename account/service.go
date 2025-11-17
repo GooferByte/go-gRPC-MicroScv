c09d808500d0ea4f06f6fef4a7b490a80fb0bb7c
GooferByte <devasrivastava21@gmail.com>
2025-11-18 02:28:06 +0530

    Updated service.go file

---
+++ account/service.go
@@ -0,0 +1,34 @@
+package account
+
+import "context"
+
+type Service interface {
+	PostAccount(ctx context.Context, name string) (*Account, error)
+	GetAccount(ctx context.Context, id string) (*Account, error)
+	GetAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error)
+}
+
+type Account struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type accountService struct {
+	repository Repository
+}
+
+func newService(r Repository) Service {
+	return &accountService{r}
+}
+
+func (s *accountService) PostAccount(ctx context.Context, name string) (*Account, error) {
+	s.repository.PutAccount()
+}
+
+func (s *accountService) GetAccount(ctx context.Context, id string) (*Account, error) {
+	s.repository.GetAccountByID()
+}
+
+func (s *accountService) GetAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error) {
+	s.repository.ListAccounts()
+}
