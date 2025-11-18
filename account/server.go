a2f5b7a5cd6266f81cb938c0333f5a4bcdd90417
GooferByte <devasrivastava21@gmail.com>
2025-11-18 23:52:28 +0530

    Updated the account.proto file

--- account/server.go
+++ account/server.go
@@ -23,27 +23,44 @@ func ListenGRPC(s Service, port int) error{
 	reflection.Register(serv)
 	return serv.Server(lis)
 }
-func (s *grpcServer) PostAccount(ctx context.Context, r *pb.)(*pb., error){
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.PostAccountRequest)(*pb.PostAccountResponse, error){
 	a, err := s.service.PostAccount(ctx, r.Name)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.{}
+	return &pb.PostAccountResponse{Account: &pb.Account{
+		Id: a.ID,
+		Name: a.Name,
+	}}, nil
 }
 
-func (s *grpcServer) GetAccount(ctx context.Context, r *pb.) (*pb., error){
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.GetAccountRequest) (*pb.GetAccountRespose, error){
 	a, err := s.service.GetAccount(ctx, r.Id)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.{}
+	return &pb.GetAccountRespose{
+		Account: &pb.Account{
+			Id: a.ID,
+			Name: a.Name,
+		},
+	}, nil
 }
 
-func (s *grpcServer) GetAccounts(ctx context.Context, r.*pb.) (*pb., error){
+func (s *grpcServer) GetAccounts(ctx context.Context, r.*pb.GetAccountsRequest) (*pb.GetAccountsRespose, error){
 	a, err := s.service.GetAccounts(ctx, r.Id)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.{}
+	accounts := []*pb.Account{}
+	for _, p := range res {
+		accounts = append(accounts, 
+			&pb.Account{
+				Id: p.ID,
+				Name: p.name,
+			},
+		)
+	}
+	return &pb.GetAccountsRespose{Accounts: accounts}, nil
 }
 
--- go.mod
+++ go.mod
@@ -6,24 +6,22 @@ toolchain go1.24.8
 
 require (
 	github.com/99designs/gqlgen v0.17.81
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/segmentio/ksuid v1.0.4
 	github.com/vektah/gqlparser/v2 v2.5.31
-	golang.org/x/text v0.30.0
+	google.golang.org/grpc v1.77.0
 )
 
 require (
 	github.com/agnivade/levenshtein v1.2.1 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
-	github.com/kelseyhightower/envconfig v1.4.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
-	github.com/urfave/cli/v2 v2.27.7 // indirect
-	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
-	golang.org/x/mod v0.28.0 // indirect
-	golang.org/x/sync v0.17.0 // indirect
-	golang.org/x/tools v0.37.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	golang.org/x/net v0.46.1-0.20251013234738-63d1a5100f82 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251022142026-3a174f9686a8 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 )
