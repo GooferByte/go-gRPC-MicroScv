56c462752b508764fd9c1558f1d9f77cdeae9ed2
GooferByte <devasrivastava21@gmail.com>
2025-11-18 23:21:44 +0530

    Updated the server.go

---
+++ account/server.go
@@ -0,0 +1,49 @@
+package account
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type fgrpcServer struct{
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error{
+	lis, err := net.Listen("tcp", fmt.Sprint(":%d", port))
+	if err != nil {
+		return err
+	}
+	serv := grpc.NewServer()
+	pb.(serv,)
+	reflection.Register(serv)
+	return serv.Server(lis)
+}
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.)(*pb., error){
+	a, err := s.service.PostAccount(ctx, r.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.{}
+}
+
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.) (*pb., error){
+	a, err := s.service.GetAccount(ctx, r.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.{}
+}
+
+func (s *grpcServer) GetAccounts(ctx context.Context, r.*pb.) (*pb., error){
+	a, err := s.service.GetAccounts(ctx, r.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.{}
+}
+
--- account/service.go
+++ account/service.go
@@ -1,6 +1,10 @@
 package account
 
-import "context"
+import (
+	"context"
+
+	"github.com/segmentio/ksuid"
+)
 
 type Service interface {
 	PostAccount(ctx context.Context, name string) (*Account, error)
@@ -22,13 +26,24 @@ func newService(r Repository) Service {
 }
 
 func (s *accountService) PostAccount(ctx context.Context, name string) (*Account, error) {
-	s.repository.PutAccount()
+	a := &Account{
+		Name: name,
+		ID:   ksuid.New().String(),
+	}
+	if err := s.repository.PutAccount(ctx, *a); err != nil {
+		return nil, err
+	}
+	return a, nil
 }
 
 func (s *accountService) GetAccount(ctx context.Context, id string) (*Account, error) {
-	s.repository.GetAccountByID()
+	return s.repository.GetAccountByID(ctx, id)
 }
 
 func (s *accountService) GetAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error) {
-	s.repository.ListAccounts()
+	if take > 100 || (skip == 0 && take == 0) {
+		take = 100
+	}
+
+	return s.repository.ListAccounts(ctx, skip, take)
 }
